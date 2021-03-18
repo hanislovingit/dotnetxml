9f337b21b275060ab9d8a878a35cb6c4035ad04d
Chuck Han <HanC@b2b.regn.net>
2021-03-17 19:01:52 -0500

    create new .net core console app

---
+++ DotNetXml/XmlSerialization/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XmlSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
